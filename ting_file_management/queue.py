adae68a58ec1e614f5a64e3dbe4231fa8adf485a
Felipe Gama Danta <epilefgama2@gmail.com>
2022-12-01 15:24:38 -0300

    refact: queue name

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,17 +1,17 @@
 class Queue:
     def __init__(self):
-        self.value = []
+        self.queue = []
 
     def __len__(self):
-        return len(self.value)
+        return len(self.queue)
 
     def enqueue(self, value):
-        self.value.append(value)
+        self.queue.append(value)
 
     def dequeue(self):
-        return self.value.pop(0)
+        return self.queue.pop(0)
 
     def search(self, index):
-        if index > len(self.value) or index < 0:
+        if index > len(self.queue) or index < 0:
             raise IndexError
-        return self.value[index]
+        return self.queue[index]

cc14cc99d3407f5af2e83bb240b0a424789d0646
Felipe Gama Danta <epilefgama2@gmail.com>
2022-12-06 15:48:55 -0300

    feat: req 3, process file

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,22 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
 
+
+def process(path_file, instance):
+    content_list = txt_importer(path_file)
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content_list),
+        "linhas_do_arquivo": content_list
+    }
+
+    for index in range(instance.__len__()):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    instance.enqueue(dict)
+    print(f"{dict}", file=sys.stdout)
+    
 
 def remove(instance):
     """Aqui irá sua implementação"""

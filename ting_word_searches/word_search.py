5a2a7d2a1215fd890b950376e8612d194aa900a0
Felipe Gama Danta <epilefgama2@gmail.com>
2022-12-12 18:56:02 -0300

    feat: req 6, word exists

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files_with_word = list()
+    for index in range(instance.__len__()):
+        content = instance.search(index)
+        content_lines = content["linhas_do_arquivo"]
+        ocurrences = list()
+        for i in range(content_lines.__len__()):
+            if word.lower() in content_lines[i].lower():
+                ocurrences.append(i + 1)
+
+        if len(ocurrences):
+            files_with_word.append(
+                {
+                    "palavra": word,
+                    "arquivo": content["nome_do_arquivo"],
+                    "ocorrencias": [{"linha": row} for row in ocurrences],
+                }
+            )
+
+    return files_with_word
 
 
 def search_by_word(word, instance):
